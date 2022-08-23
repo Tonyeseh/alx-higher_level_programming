407ed0a02428975e369b1b71af5e589e90279b02
tony <tony@git.com>
2022-08-23 14:21:45 -0700

    solution to task 6 0x01 HLL

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+i = 0
+while i < 9:
+    j = 1
+    while j < 10:
+        print(f"{i}", end='')
+        print(f"{j}", end='')
+        if i == 8 and j ==9:
+            print("")
+        else:
+            print("", end=", ")
+        j += 1
+    i += 1
