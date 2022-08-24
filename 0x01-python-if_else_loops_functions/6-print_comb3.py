81dade1a772707a242c84772d5af41eba040c7e3
tony <tony@git.com>
2022-08-24 01:11:11 -0700

    reduced the number of print statements in task 6 0x01 HLL

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -3,8 +3,7 @@ i = 0
 while i < 9:
     j = i + 1
     while j < 10:
-        print(f"{i}", end='')
-        print(f"{j}", end='')
+        print("{}{}".format(i, j), end='')
         if i == 8 and j == 9:
             print("")
         else:
