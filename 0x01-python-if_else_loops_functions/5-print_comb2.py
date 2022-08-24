f25833e17567f91aba8c1ddac67174bda53ee5ec
tony <tony@git.com>
2022-08-24 02:33:55 -0700

    solution to task 5 0x01 HLL

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,7 +1,6 @@
 #!/usr/bin/python3
-for num in range(10):
-    for num1 in range(10):
-        if num is not 9 or num1 is not 9:
-            print("{}{}".format(num, num1), end=', ')
-        else:
-            print("{}{}".format(num, num1))
+for num in range(100):
+    if num is not 99:
+        print("{:02d}".format(num), end=', ')
+    else:
+        print("{}".format(num))
