9e32064949358c8d59a2c1f6b8efbd2a7017514b
tony <tony@git.com>
2022-08-29 13:58:22 -0700

    correction to task 9 0x03 HLL

--- 0x03-python-data_structures/9-max_integer.py
+++ 0x03-python-data_structures/9-max_integer.py
@@ -3,7 +3,7 @@ def max_integer(my_list=[]):
     if my_list is None or len(my_list) == 0:
         return None
     max_num = my_list[0]
-    for i in range(1, len(my_list) - 1):
+    for i in range(1, len(my_list)):
         if max_num < my_list[i]:
             max_num = my_list[i]
     return max_num
