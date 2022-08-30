0bc03b5f12a358ec09253112123254d6156ad6a1
tony <tony@git.com>
2022-08-29 21:28:56 -0700

    1st tried solution to task 8 0x03 HLL

---
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def multiple_returns(sentence):
+    sent_len = len(sentence)
+    if sent_len == 0:
+        first_char = None
+    else:
+        first_char = sentence[0]
+    return ((sent_len, sentence[0]))
