0bbc7b21031bbb731cc27b16f2494c84ece4b51f
tony <tony@git.com>
2022-09-02 11:29:53 -0700

    solution to task 12 0x04 HLP

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,33 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    """converts a Roman numeral to an integer
+    """
+    if type(roman_string) is str:
+        prev_i = ""
+        num = 0
+        for i in roman_string:
+            if i == "M":
+                if prev_i == "C": num += 800
+                else: num += 1000
+            elif i == "D":
+                if prev_i == "C": num += 300
+                else: num += 500
+            elif i == "C":
+                if prev_i == "X": num += 80
+                else: num += 100
+            elif i == "L":
+                if prev_i == "X": num += 30
+                else: num += 50
+            elif i == "X":
+                if prev_i == "I": num += 8
+                else: num += 10
+            elif i == "V":
+                if prev_i == "I": num += 3
+                else: num += 5
+            elif i == "I":
+                num += 1
+            else:
+                return (None)
+            prev_i = i
+        return (num)
+    return (None)
