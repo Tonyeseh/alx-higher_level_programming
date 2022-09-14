a6f963c64f75af15036bb4385822fab108e883a3
tony <tony@git.com>
2022-09-14 13:12:40 -0700

    fixed errors in task 100 0x05 HLP

--- 0x05-python-exceptions/100-safe_print_integer_err.py
+++ 0x05-python-exceptions/100-safe_print_integer_err.py
@@ -1,11 +1,10 @@
 #!/usr/bin/python3
-import sys
 def safe_print_integer_err(value):
+    from sys import stderr
     try:
         print("{:d}".format(value))
         return (True)
     except ValueError as err:
-        error_msg = "Exception: " + str(err) + "\n"
-        sys.stderr.write(error_msg)
+        print("Exception: {}".format(err), file=stderr)
         return (False)
 
