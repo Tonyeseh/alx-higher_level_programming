b6e8ef877821a8ea2b8aec5ce6fc0bc73a03c090
tony <tony@git.com>
2022-09-13 20:16:59 -0700

    added docstring to task 1 0x06 HLP

--- 0x06-python-classes/1-square.py
+++ 0x06-python-classes/1-square.py
@@ -1,5 +1,16 @@
 #!/usr/bin/python3
+""" Class Square """
+
+
 class Square:
-    """This class Square has info defines square objects"""
+    """This defines square objects
+    private instance attribute: size
+    """
+
     def __init__(self, size):
+        """ Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
         self.__size = size
