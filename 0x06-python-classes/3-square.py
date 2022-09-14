2b6a16ef95b855ea56c33c9c4b71c70ae15ab4ec
tony <tony@git.com>
2022-09-13 20:29:07 -0700

    solution to task 3 0x06 HLP

---
+++ 0x06-python-classes/3-square.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+""" Square Class """
+
+
+class Square:
+    """Defines a new square object"""
+
+    def __init__(self, size=0):
+        """Initialize a new Square.
+
+        Args:
+            size (int): The size of the new square.
+        """
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        elif size < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = size
+
+    def area(self):
+        """ Calculate and Returns the area of a square object
+
+        Args:
+            None
+        """
+        return (self.__size * self.__size)
