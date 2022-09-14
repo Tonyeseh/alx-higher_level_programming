196c2c0833eff9e0124efdc5fdca038aa4bdb30e
tony <tony@git.com>
2022-09-14 08:20:32 -0700

    solution to task 103 0x06 HLP

---
+++ 0x06-python-classes/103-magic_class.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+import math
+
+
+class MagicClass:
+
+    def __init__(self, radius):
+        if type(radius) is not int or type(radius) is not float:
+            raise("radius must be a number")
+        self.__radius = radius
+
+    def area(self):
+        return (math.pi * self.__radius ** 2)
+
+    def circumference(self):
+        return (2 * math.pi * self.__radius)
