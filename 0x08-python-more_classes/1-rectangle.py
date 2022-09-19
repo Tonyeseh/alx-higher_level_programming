77107f11345b51b85d60a79af4e8ea2069ab4ade
tony <tony@git.com>
2022-09-19 08:03:23 -0700

    solution to task 1 0x08 HLP

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,47 @@
+#!/usr/bin/python3
+""" Defines a Rectangle Class """
+
+
+class Rectangle:
+    """ Represent a Rectangle object """
+    def __init__(self, width=0, height=0):
+        """ Initialize a new Rectangle object
+
+        Args:
+            width (int): Width of object
+            height (int): height of object
+        """
+        self.width = width
+        self.height = height
+
+    @property
+    def width(self):
+        """ Getter for width attribute """
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """ Setter for height attribute it also validates it """
+        if not isinstance(value, int):
+            raise TypeError("width must be an integer")
+        if value < 0:
+            raise ValueError("width must be >= 0")
+        self.__width = value
+
+    @property
+    def height(self):
+        """ Getter for height attribute """
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """ Setter of height attibute.
+        
+        Args:
+            value (int): height of new object
+        """
+        if not isinstance(value, int):
+            raise TypeError("height must be an integer")
+        if value < 0:
+            raise ValueError("height must be >= 0")
+        self.__height = value
