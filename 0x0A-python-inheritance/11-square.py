a97f7d6a2badf5f942a23b9bbe0b336637414713
tony <tony@git.com>
2022-09-26 10:33:26 -0700

    solution to task 11 0x0A HLP

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" Defines a Square class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ Square class inherits from Rectangle class """
+
+    def __init__(self, size):
+        """ Initialises a new Square
+
+            Args:
+                size (int): size of square
+        """
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
+
+    def __str__(self):
+        """ Print() format for a Square object """
+        return ("[{}] {}/{}".format(type(self).__name__, self.__size, self.__size))
