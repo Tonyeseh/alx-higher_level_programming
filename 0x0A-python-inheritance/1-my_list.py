61b06054d92d2608738afe956edb29d41890e0c6
tony <tony@git.com>
2022-09-26 07:22:54 -0700

    solution tot task 1 0x0

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -1,2 +1,12 @@
 #!/usr/bin/python3
-""" Module defines 
+""" Module defines ``MyList`` class """
+
+class MyList(list):
+    """ Extention of list class
+
+        Adds a print_sorted() function
+    """
+
+    def print_sorted(self):
+        """ Prints the list but sorted(ascending sort)"""
+        print(sorted(self))
