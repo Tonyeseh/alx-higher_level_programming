aab0662cf5f529178a7256c69dcdc81a312e776a
tony <tony@git.com>
2022-09-26 08:44:45 -0700

    slution to task 6 0x0A HLP

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+""" Defines a BaseGeometry Class """
+
+class BaseGeometry():
+    """ Defines a class BaseGeometry based on 5-base_geometry.py"""
+    
+    def area(self):
+        """ Raises a NameError """
+        raise Exception("area() is not implemented")
