d4edad1ad0595d47a7940cc763dc96283a987cf2
tony <tony@git.com>
2022-09-26 08:45:47 -0700

    solution to task 4 0x0A HLP

---
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Module defines an inherited class-checking function"""
+
+def inherits_from(obj, a_class):
+    """ Checks if an object is a inherited instance of a class.
+
+        Args:
+            obj (any): Object
+            a_class (type): class type.
+
+        Returns:
+            If obj is an inherited instance of a_class - True
+            Otherwise - False
+    """
+    return issubclass(type(obj), a_class) and type(obj) != a_class
