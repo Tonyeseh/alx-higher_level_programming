0dc72370de5296e80c65d71bb7ad3378914eea24
tony <tony@git.com>
2022-09-27 05:02:17 -0700

    solution to task 3 0x0B HLP

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines ``to_json_string()`` function"""
+
+""" Uses json module """
+import json
+
+def to_json_string(my_obj):
+    """ Returns the JSON representation of an object (string)
+
+        Args:
+            my_obj (any object): object to be converted
+    """
+    json_str = json.dumps(my_obj)
+    return (json_str)
