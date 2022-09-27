cf9ceae8b85566df69e0b830208279b07eed742f
tony <tony@git.com>
2022-09-27 05:01:19 -0700

    solution to task 2 0x0B HLP

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Defines ``append_write()`` function"""
+
+def append_write(filename="", text=""):
+    """ appends astring at the end of a text file UTF-8
+
+        Args:
+            filename (string): name of file, sometimes contains path
+            text (string): text to add to file
+
+        Returns: the number of characters added
+    """
+    with open(filename, "a", encoding="utf-8") as f:
+        num_of_char = f.write(text)
+        return (num_of_char)
