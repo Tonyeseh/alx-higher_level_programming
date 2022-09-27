98cae58e502a994681dfd20d5c6b5571d4b426b7
tony <tony@git.com>
2022-09-27 05:03:26 -0700

    empty files for all mandatory tasks 0x0B

---
+++ 0x0B-python-input_output/4-from_json_string.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" Defines ``from_json_string()`` function """
+
+""" Uses json module """
+import json
+
+def from_json_string(my_str):
+    """ Returns an object(Python data stucture)
+    represented by a JSON string
+
+        Args:
+            my_str (string): JSON string
+    """
+    return (json.load(my_str))
