aac278cea6664c42b260ce0f16f3990d37279d04
tony <tony@git.com>
2022-09-27 07:03:31 -0700

    solution to task 7 0x0B HLP

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+""" Script that adds all arguments to a python lsit and then
+save them to a file """
+
+""" Importing modules """
+import sys
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+my_list = []
+for arg in range(1, len(sys.argv)):
+    my_list.append(sys.argv[arg])
+
+try:
+    lst = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    lst = []
+lst += my_list
+save_to_json_file(lst, "add_item.json")
