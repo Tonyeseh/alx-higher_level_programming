e522e18c3d063d6d9d41313e54e2513aff75c5f0
tony <tony@git.com>
2022-09-27 05:00:36 -0700

    solution to task 1 0x0B HLP

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" Defines write_file(...) function """
+
+def write_file(filename="", text=""):
+    """ writes to a string to a text file
+
+        Args:
+            filename (string): name of file
+            text (string): string to write to file ``filename``
+
+        Returns: Number of characters written
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        num_of_char = f.write(text)
+        return (num_of_char)
