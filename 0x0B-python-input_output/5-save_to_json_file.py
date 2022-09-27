9aa1bc8206abc5058b8be919948753f0a401e1be
tony <tony@git.com>
2022-09-27 05:33:38 -0700

    solution to task 5 0x0B HLP

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" Defines ``save_to_json(...)`` function """
+
+""" Uses json module """
+import json
+
+def save_to_json_file(my_obj, filename):
+    """ writes an Object to a text file, using JSON representation
+
+        Args:
+            my_obj (object): preferrable serialisable object
+            filename (string): file to dump the JSON string
+    """
+    with open(filename, "w", encoding="utf-8") as f:
+        json_string = json.dumps(my_obj)
+        f.write(json_string)
