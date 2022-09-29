aa1282a9819f2454750d5ea0432f08ffe56b22f0
tony <tony@git.com>
2022-09-29 06:12:24 -0700

    solution to task 1 0x0C HLP

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Defines a class Base """
+
+
+class Base:
+    """ Base class
+
+        Args:
+            __nb_objects (int): number of objects
+    """
+    __nb_objects = 0
+
+
+    def __init__(self, id=None):
+        """ Initialises the Class Base
+
+            Args:
+                id (int): integer id
+        """
+        if id is not None:
+            self.id = id
+
+        else:
+            type(self).__nb_objects += 1
+            self.id = type(self).__nb_objects
