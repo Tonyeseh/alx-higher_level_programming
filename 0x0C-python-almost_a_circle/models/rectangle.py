52c858e50b444427be334aa737789726bc2f9f7a
tony <tony@git.com>
2022-10-07 13:53:17 -0700

    corrected  validator error

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -95,7 +95,7 @@ class Rectangle(Base):
             raise TypeError("y must be an integer")
 
         if value < 0:
-            raise ValueError("y must be > 0")
+            raise ValueError("y must be >= 0")
         self.__y = value
 
     def area(self):
