375d349f6a825d957195de9fe653fbe39d8aa1b6
tony <tony@git.com>
2022-11-01 16:01:55 -0700

    solution to task 11 0x0D

---
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -0,0 +1,3 @@
+-- lists the records with a score >= 10 in the table second table
+-- displayed in descending order
+SELECT `score`, `name` FROM `second_table` WHERE `score` >= 10 ORDER BY `score` DESC;
