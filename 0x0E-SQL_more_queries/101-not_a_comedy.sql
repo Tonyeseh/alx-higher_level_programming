60cfb19035ed46b6ec4d7107b1f70d6aa8c763c5
tony <tony@git.com>
2022-11-03 19:34:18 -0700

    solution to task 101 0x0E

---
+++ 0x0E-SQL_more_queries/101-not_a_comedy.sql
@@ -0,0 +1,12 @@
+-- lists all genres of the show Dexter
+SELECT tv_shows.title
+FROM tv_shows
+WHERE tv_shows.title NOT IN
+	(SELECT tv_shows.title
+	FROM tv_genres
+	JOIN tv_show_genres
+	ON tv_genres.id = tv_show_genres.genre_id
+	JOIN tv_shows
+	ON tv_shows.id = tv_show_genres.show_id
+	WHERE tv_genres.name = 'Comedy')
+ORDER BY tv_shows.title;
