d90640759e3af1093a1273e6db3dacf3e7886916
tony <tony@git.com>
2022-11-03 20:29:50 -0700

    correction to tasks 15 and 16 0x0D

--- 0x0D-SQL_introduction/15-groups.sql
+++ 0x0D-SQL_introduction/15-groups.sql
@@ -1,5 +1,5 @@
 -- lists the number of records with the same score in the table second_table
-SELECT score, COUNT(score) AS 'number'
+SELECT score, COUNT(score) AS number
 FROM second_table
 GROUP BY score
-ORDER BY 'number' DESC;
+ORDER BY number DESC;
--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -2,5 +2,5 @@
 -- rows without a name value are not listed
 SELECT score, name
 FROM second_table
-WHERE name IS NOT NULL;
+WHERE name IS NOT NULL
 ORDER BY score DESC;
