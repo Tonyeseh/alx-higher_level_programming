fb3ddce502d6cf31fad696b98162af88c9969bcb
tony <tony@computer.com>
2022-11-29 16:29:52 +0100

    solution to task 6 0x013

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const Rectangle = require('./4-rectangle');
+
+module.exports = class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+  }
+
+  charPrint (c) {
+    if (typeof c === 'undefined') {
+      super.print();
+    } else {
+      let string;
+      for (let i = 0; i < this.height; i++) {
+        string = '';
+        for (let j = 0; j < this.width; j++) { string = string + c; }
+        console.log(string);
+      }
+    }
+  }
+};
