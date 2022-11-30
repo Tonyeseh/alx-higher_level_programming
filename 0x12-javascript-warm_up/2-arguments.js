130b48bb8c63e0692da1406e9411c9e88ba2ecb9
tony <tony@computer.com>
2022-11-30 12:09:24 +0100

    updated task 2 0x12
    corrected the output string

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -3,9 +3,9 @@
 if (process.argv.length <= 2) {
   console.log('No argument');
 } else {
-  if (process.argv.length > 3) {
+  if (process.argv.length == 3) {
     console.log('Argument found');
   } else {
-    console.log('Argument found');
+    console.log('Arguments found');
   }
 }
