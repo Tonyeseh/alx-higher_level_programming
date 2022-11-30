3337f433e3cea5000c93ca77acde03338b3a0dfb
tony <tony@computer.com>
2022-11-30 12:47:47 +0100

    fixed semistandard errors to task 13 0x013

--- 0x13-javascript_objects_scopes_closures/102-concat.js
+++ 0x13-javascript_objects_scopes_closures/102-concat.js
@@ -2,7 +2,7 @@
 
 const fs = require('fs');
 
-file1 = fs.readFileSync(process.argv[2], 'utf8');
-file2 = fs.readFileSync(process.argv[3], 'utf-8');
+const file1 = fs.readFileSync(process.argv[2], 'utf8');
+const file2 = fs.readFileSync(process.argv[3], 'utf-8');
 
 fs.writeFileSync(process.argv[4], file1 + file2);
