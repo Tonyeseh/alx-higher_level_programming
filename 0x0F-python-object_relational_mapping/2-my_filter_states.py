3d4e603d2ecbac3783f8980dd26ac95d9087cb1a
tony <tony@git.com>
2022-12-21 03:47:48 -0800

    correction to task 2 0x0f

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -12,7 +12,8 @@ import MySQLdb
 if __name__ == "__main__":
     db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
     c = db.cursor()
-    query = "SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4])
+    query = "SELECT * FROM `states`\
+            WHERE BINARY `name` = '{}'".format(sys.argv[4])
     c.execute(query)
     for state in c.fetchall():
         print(state)
