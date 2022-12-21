194dda1e7fe624de7e12e8c965fac5cb582206d5
tony <tony@git.com>
2022-12-21 03:19:08 -0800

    solution to task 2 0x0f

---
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -0,0 +1,18 @@
+#!/usr/bin/python3
+"""
+displays all values in state table where name matches
+    user argument in database hbtn_0e_0_usa
+Usage: ./2-my_filter_states.py <db_username> <db_password> <db_name>\
+        <state to be found>
+"""
+
+import sys
+import MySQLdb
+
+if __name__ == "__main__":
+    db = MySQLdb.connect(user=sys.argv[1], passwd=sys.argv[2], db=sys.argv[3])
+    c = db.cursor()
+    query = "SELECT * FROM `states` WHERE `name` = '{}'".format(sys.argv[4])
+    c.execute(query)
+    for state in c.fetchall():
+        print(state)
