12157cb2d0d2e58f68557fabab659cde48897d5b
tony <tony@computer.com>
2023-01-27 19:46:44 +0100

    solution to adv task 11 0x15

---
+++ 0x15-javascript-web_jquery/101-script.js
@@ -0,0 +1,11 @@
+$(function () {
+  $('div#add_item').click(() => {
+    $('ul.my_list').append('<li>Item</li>');
+  });
+  $('div#remove_item').click(() => {
+    $('ul.my_list li').last().remove();
+  });
+  $('div#clear_list').click(() => {
+    $('ul.my_list').children().remove();
+  });
+});
